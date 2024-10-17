3b6759e7e669ac40ef2aafea050ad51b0e9f989a
Rushikesh-guthub <164461195+Rushikesh-guthub@users.noreply.github.com>
2024-10-17 16:21:21 +0530

    Update Dashboard.js

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Dashboard = () => {
     return (
       <div>
-        <h2>Dashboard</h2>
+        <h2>Dashboard2</h2>
         <Link to="/register">Register</Link>
       </div>
     );
