efdd9087e7ff3edfebdbed80ae89b4813eb5971a
Rushikesh-guthub <164461195+Rushikesh-guthub@users.noreply.github.com>
2024-11-07 18:28:22 +0530

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div>
-      <h2>Welcome Home19</h2>
+      <h2>Welcome Home122</h2>
       <Link to="/login">Login</Link> | <Link to="/register">Register</Link>
     </div>
   );
